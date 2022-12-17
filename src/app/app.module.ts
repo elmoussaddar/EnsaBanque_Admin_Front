81bfe4ec20a37811e83075ea119ee8e886b04822
elmoussaddar <mounimfitri@gmail.com>
2022-12-17 02:08:35 +0100

    cleanup mounim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ListAgentsComponent } from './list-agents/list-agents.component';
+import { AgentProfileComponent } from './agent-profile/agent-profile.component';
+import { AgentAddFormComponent } from './agent-add-form/agent-add-form.component';
+import { FormsModule } from '@angular/forms';
+import { StartViewComponent } from './start-view/start-view.component';
+import { HttpClientModule } from '@angular/common/http';
+import { authInterceptorProviders } from '../_helpers/auth.interceptor';
+import { AdminLoginFormComponent } from './Components/admin-login-form/admin-login-form.component';
+import { AdminHomeComponent } from './Components/admin-home/admin-home.component';
+import { AdminHeaderComponent } from './Components/admin-header/admin-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ListAgentsComponent,
+    AgentProfileComponent,
+    AgentAddFormComponent,
+    AdminLoginFormComponent,
+    AdminHomeComponent,
+    AdminHeaderComponent,
+    StartViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
